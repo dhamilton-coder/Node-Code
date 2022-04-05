82d4faad176232a90687895e463249845346f192
dhamilton-coder <61112404+dhamilton-coder@users.noreply.github.com>
2022-04-05 12:19:09 +0100

    Add files via upload

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,27 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/weather')
 
-const url = 'http://api.weatherstack.com/current?access_key=ef02a94adb30ec49eb62ca75d4e90ebe&query=37.8267,-122.4233'
 
-request({ url: url}, function (error, response) {
-        const data = JSON.parse(response.body)
-            console.log(data.current)
+adress = process.argv[2]
+if (!adress) {
+    console.log('Please provide an adress! ("Example Adress")')    
+}else {
+
+geocode(adress, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+forecast(data.latitude, data.longtitude, (error, response) => {
+       if (error)  {
+                return console.log(error)
+       }
+       console.log(' ')
+       console.log(data.location) 
+       console.log(response)
+       console.log(' ')
+        
 })
+})
+}
+
