b0f21a4f8a718b1c1d203fb53fdb742cd3f6ffb3
dhamilton-coder <61112404+dhamilton-coder@users.noreply.github.com>
2022-04-23 15:40:15 +0100

    Public Dir for Static html and css

---
+++ web-server/public/js/app.js
@@ -0,0 +1,36 @@
+console.log('Client side Javascript File is Loaded!!')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageA = document.querySelector('#mA')
+const messageB = document.querySelector('#mB')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const loca = search.value
+
+    fetch('http://localhost:3000/weather?address=' + loca).then((response) => {
+        response.json().then((data) => {
+           if (data.error) {
+            messageA.textContent = 'Error'
+            messageB.textContent = data.error
+           }else {
+            messageA.textContent = data.location
+            messageB.textContent = data.forecast
+            
+           }
+        })
+})
+
+
+
+   
+})
+
+
+
+
+
