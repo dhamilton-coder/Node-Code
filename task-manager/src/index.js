f661fe047adb6b763f81a8c64e491e2486c107ef
dhamilton-coder <61112404+dhamilton-coder@users.noreply.github.com>
2022-06-20 16:53:29 +0100

    Updated Index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,13 +9,21 @@ require('./db/mongoose.js')
 const User = require('./models/user.js')
 const Task = require('./models/task.js')
 const { ObjectID } = require('bson')
+const userRouter = require('./routers/user.js')
+const taskRouter = require('./routers/task.js')
 
 
 
-//Ports and App
+//Port and App constants
 const app = express()
 const port = process.env.PORT || 3000
 
+//Telling Express to automaticly parse incoming JSON
+app.use(express.json())
+
+//Setting up Router for /User and /Task Routes
+app.use(userRouter)
+app.use(taskRouter)
 
 
 //Define paths
@@ -37,14 +45,8 @@ app.set("view engine", "hbs");
 
 // Setup Static Dir to use
 app.use(express.static(publicDir));
-//Telling Express to automaticly parse incoming JSON
-app.use(express.json())
-
-
 
 //Routes 
-
-
 app.get("/", (req, res) => {
     res.render("main" , {
         title: 'Main Page',
@@ -52,82 +54,6 @@ app.get("/", (req, res) => {
         layout: false
     })
 })
-
-
-app.post("/users", (req, res) => {
-    const user = new User(req.body)
-
-    user.save().then(() => {
-        res.send(user).catch((error) => {
-        res.status(400)
-        res.send(error)
-        })
-    })
-})
-
-
-app.get("/users", (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users)
-    }).catch((e) => {
-        res.status(500)
-        res.send(e)
-    })
-})
-
-
-app.get("/users/:id", (req, res) => {
- const _id = req.params.id
- User.findById(_id).then((user) => {
-    if (!user) {
-    return res.status(404)
-    }
-    res.send(user)    
- }).catch((e) => {
-    res.send(e)
- })
- 
-console.log(req.params)
-
-})
-
-app.post("/tasks", (req, res) => {
-    const task = new Task(req.body)
-
-    task.save().then(() => {
-        res.status(201)
-        res.send(task)
-    }).catch((e) => {
-        res.status(400)
-        res.send(e)
-    })
-})
-
-
-app.get("/tasks", (req, res) => {
-    Task.find({}).then((users) => {
-        res.send(users)
-    }).catch((e) => {
-        res.status(500)
-        res.send(e)
-    })
-})
-
-app.get("/tasks/:id", (req, res) => {
-    const _id = req.params.id
-
-    Task.findById(_id).then((user) => {
-        if (!user) {
-    return res.status(404).send()
-        }
-    res.send(user)
-    }).catch((e) => {
-        res.status(500)
-        res.send(e)
-    })
-})
-
-
 
 
 app.listen(port, () => {
