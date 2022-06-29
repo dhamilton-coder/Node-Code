a1df08a1e0b6514745719bc6b4996c08a68b945d
dhamilton-coder <61112404+dhamilton-coder@users.noreply.github.com>
2022-06-29 10:31:42 +0100

    Added Option to Delete User Profile

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,159 +1,134 @@
 
 //Imports
 
-const express = require('express')
-const User = require('../models/user')
-const auth = require('../middleware/auth')
+const mongoose = require('mongoose')
+const mongo = require('mongodb')
+const mongoClient = mongo.MongoClient
 const bcrypt = require('bcryptjs')
-const router = new express.Router()
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
+//Schema to pass into Model
 
-//Route for Creating new User via http POST request
+const UserSchema = new mongoose.Schema( {
+    name: {
+       type: String,
+       required: true,
+       trim: true,
+       lowercase: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
 
-router.post("/users", async (req, res) => {
-    const user = new User(req.body)
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('lol not a flippin email')
+            }
+        },
+       
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase() === 'password') {
+               throw new Error('tsk tsk tsk')
+            }
+        }
+    },
 
-    try {
-        await user.save()
-        res.send(user)
-        res.status(201)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-
-  
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a possitive number!')
+            }
+        }
+    }, 
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
-//Route for logging in Users via their email and password
-
-router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({ user , token })
-    } catch (e) {
-        res.status(400).send()
-    }
+UserSchema.virtual('tasks', {
+    ref: 'task',
+    localField: '_id',
+    foreignField: 'author'
 })
 
-//Route for signing up users
+UserSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
 
-router.post('/users/signin', async (req, res) => {
-    try {
-        const user = await new User(req.body)
-        await user.save()
-        const token = await user.generateAuthToken()
-        res.status(201)
-        res.send({ user, token })
+    delete userObject.password
+    delete userObject.tokens
 
-    } catch (e) {
-        res.status(400).send()
-    }
-})
-
-//Route for Logging out users by deleting Session ID token
-
-router.post('/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
-        })
-        await req.user.save()
-
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-router.post('/users/logoutAll', auth,  async (req, res) => {
-    try {
-        req.user.tokens = []
-        req.user.save()
-        res.send()
-    } catch(e) {
-        res.status(500).send()
-    }
-})
-
-
-//Route for Reading A User Profile via their Session ID token
-
-router.get("/users/me", auth, async (req, res) => {
- res.send(req.user)
-})
-    
-//Route for Reading 1 User from Users Collection via their _id by a http GET request 
-
-router.get("/users/:id", async (req, res) => {
- const _id = req.params.id
- 
- 
- User.findById(_id).then((user) => {
-
-    if (!user) {
-    return res.status(404).send()
-    }
-
-    res.send(user)    
- }).catch((e) => {
-    res.send(e)
- })
- 
-console.log(req.params)
-
-})
-
-//Route for Updating a User from the Users collection via http PATCH request
-
-router.patch('/users/:id', async (req, res) => {
-const _id = req.params.id
-const updates = Object.keys(req.body)
-const allowedUpdates = ['name', 'email', 'password', 'age']
-const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-   
-if (!isValidOperation) {
-    return res.status(400).send( { error : 'Invalid Updates'} )
+    return userObject
 }
 
-try {
-    const user = await User.findById(_id)
+
+UserSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
 
     if (!user) {
-        return  res.status(404).send()
-     }
+        throw new Error('Unable to login')
+    }
 
-    updates.forEach((update) => {
-        user[update] = req.body[update]
-    })
+    const isMatch = await bcrypt.compare(password, user.password)
 
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+UserSchema.methods.generateAuthToken = async function ()  {
+    const user = this
+    const token = jwt.sign({ _id : user._id.toString() }, 'thisismynewcourse')
+
+    user.tokens = user.tokens.concat({ token })
     await user.save()
-        res.send(user)
-}   catch (e) {
-    res.status(400).send()
+
+    return token
 }
+
+UserSchema.pre('save', async function (next) {
+const user = this
+
+if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+}
+
+console.log('just before saving')
+
+    next()
 })
 
-//Route for deleting users via their id
-
-router.delete('/users/:id', async (req, res) => {
-    try {
-       
-        const user = await User.findByIdAndDelete(req.params.id)
-       
-        if (!user) {
-        return res.status(404).send()
-       }
-        
-       res.send(user)
-    } catch (e) {
-       res.status(500).send()
-    }
+UserSchema.pre('remove', async function (next)  {
+const user = this
+await Task.deleteMany({ author : user._id})
+next()
 })
 
 
 
+//Create New Model Using Mongoose
+
+const User = mongoose.model('User', UserSchema)
 
 
 
-module.exports = router
+
+module.exports = User
