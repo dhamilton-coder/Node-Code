6b5763a77fd7ba26230cbc84f57ef462901c7bd5
dhamilton-coder <61112404+dhamilton-coder@users.noreply.github.com>
2022-07-07 13:28:00 +0100

    Created two email functions to call in routers

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,10 +1,32 @@
 const sgMail = require('@sendgrid/mail')
 
 const apiKey = ''
-
 sgMail.setApiKey(apiKey)
 
-sgMail.send({
-    to: '' ,
-    
-})
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'davidpatrick1000r@gmail.com',
+        subject: 'Thanks for joining',
+        text: `Thank you for creating an account ${name}` 
+    })
+}
+
+
+const sendCancelEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'davidpatrick1000r@gmail.com',
+        subject: 'Follow up (why did you delete account)',
+        text: `Hi, ${name}, We just wanted to follow up on why you deleted your account with us.
+                We would love some feedback on how to better improve our product and how in future we could get you to stay.`
+
+    })
+}
+
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelEmail
+}
+
